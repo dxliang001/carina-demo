85104a38045c5f88ea15b2a9f94421ff43950241
dxliang001 <dongxinliang6@gmail.com>
2023-07-24 14:25:55 -0400

    Final Hw Checked

--- src/main/java/com/zebrunner/carina/demo/gui/saucedemo/MainPage.java
+++ src/main/java/com/zebrunner/carina/demo/gui/saucedemo/MainPage.java
@@ -4,17 +4,27 @@ import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainPage extends AbstractPage {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private WebDriver driver;
 
+    @FindBy(className = "inventory_list")
+    private ExtendedWebElement inventoryList;
     @FindBy(id = "react-burger-menu-btn")
     private ExtendedWebElement menuButton;
-
+    
     @FindBy(id = "logout_sidebar_link")
     private ExtendedWebElement logoutButton;
 
@@ -26,6 +36,14 @@ public class MainPage extends AbstractPage {
 
     @FindBy(xpath = "//select[@class='product_sort_container']")
     private ExtendedWebElement sortButton;
+
+    @FindBy(className = "shopping_cart_badge")
+    private ExtendedWebElement cartBadge;
+
+    @FindBy(id = "about_sidebar_link")
+    private ExtendedWebElement aboutLink;
+
+
 
     public MainPage(WebDriver driver) {
         super(driver);
@@ -62,50 +80,84 @@ public class MainPage extends AbstractPage {
         return itemPriceElement.getText();
     }
 
-
-    public void clickAddToCartButton(String itemId) {
-        WebElement addToCartButton = driver.findElement(By.id("add-to-cart-" + itemId));
-        addToCartButton.click();
+    public boolean isItemInCart() {
+        try {
+            ExtendedWebElement cartBadge = findExtendedWebElement(By.className("shopping_cart_badge"));
+            int itemCount = Integer.parseInt(cartBadge.getText());
+            return itemCount > 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
-    public boolean isItemInCart(String itemId) {
-        WebElement cartItem = driver.findElement(By.id("cart-" + itemId));
-        return cartItem != null;
+    public boolean isInventoryListPresent() {
+        try {
+            assertElementPresent(inventoryList);
+            List<ExtendedWebElement> inventoryItems = inventoryList.findExtendedWebElements(By.className("inventory_item"));
+            if (inventoryItems.isEmpty()) {
+                return false;
+            } else {
+                for (ExtendedWebElement item : inventoryItems) {
+                    ExtendedWebElement itemNameElement = item.findExtendedWebElement(By.className("inventory_item_name"));
+                    LOGGER.info(itemNameElement.getText());
+                }
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 
-    public boolean isProductCardDisplayed(String itemId) {
-        WebElement productCard = driver.findElement(By.id("product-card-" + itemId));
-        return productCard != null;
+    public void selectSortOption(String optionValue) {
+        Select sortSelect = new Select(sortButton.getElement());
+        sortSelect.selectByValue(optionValue);
     }
 
-    public void searchForItem(String itemName) {
-        WebElement searchBox = driver.findElement(By.id("search-box"));
-        searchBox.sendKeys(itemName);
-        searchBox.submit();
+    public String getSortOption() {
+        Select sortSelect = new Select(sortButton.getElement());
+        return sortSelect.getFirstSelectedOption().getText();
     }
 
-    public boolean isItemDisplayedInSearchResults(String itemName) {
-        WebElement searchResult = driver.findElement(By.xpath("//div[contains(text(), '" + itemName + "')]"));
-        return searchResult != null;
+    public boolean isSorted(String optionValue) throws InterruptedException {
+        List<ExtendedWebElement> inventoryItems = inventoryList.findExtendedWebElements(By.className("inventory_item"));
+        List<String> itemNames = new ArrayList<>();
+        List<Double> itemPrices = new ArrayList<>();
+
+        for (ExtendedWebElement item : inventoryItems) {
+            ExtendedWebElement itemNameElement = item.findExtendedWebElement(By.className("inventory_item_name"));
+            itemNames.add(itemNameElement.getText());
+
+            ExtendedWebElement itemPriceElement = item.findExtendedWebElement(By.className("inventory_item_price"));
+            itemPrices.add(Double.parseDouble(itemPriceElement.getText().substring(1))); // remove the $ sign and parse to double
+        }
+
+        if (optionValue.equals("az") || optionValue.equals("za")) {
+            List<String> sortedItemNames = new ArrayList<>(itemNames);
+            Collections.sort(sortedItemNames);
+            if (optionValue.equals("za")) {
+                Collections.reverse(sortedItemNames);
+
+            }
+            return itemNames.equals(sortedItemNames);
+        } else if (optionValue.equals("lohi") || optionValue.equals("hilo")) {
+            List<Double> sortedItemPrices = new ArrayList<>(itemPrices);
+            Collections.sort(sortedItemPrices);
+            if (optionValue.equals("hilo")) {
+                Collections.reverse(sortedItemPrices);
+
+            }
+            return itemPrices.equals(sortedItemPrices);
+        } else {
+            return false;
+        }
     }
 
-    public void openAllItemsPage() {
-        WebElement allItemsLink = driver.findElement(By.id("all-items-link"));
-        allItemsLink.click();
+    public void openMenu() {
+        menuButton.click();
     }
 
-    public boolean isAllItemsPageDisplayed() {
-        WebElement allItemsPage = driver.findElement(By.id("all-items-page"));
-        return allItemsPage != null;
-    }
-
-    public void openAboutPage() {
-        WebElement aboutLink = driver.findElement(By.id("about-link"));
+    public void openAboutSite() {
         aboutLink.click();
     }
 
-    public boolean isAboutPageDisplayed() {
-        WebElement aboutPage = driver.findElement(By.id("about-page"));
-        return aboutPage != null;
-    }
 }
--- src/test/java/com/zebrunner/carina/demo/saucedemo/MainPageTest.java
+++ src/test/java/com/zebrunner/carina/demo/saucedemo/MainPageTest.java
@@ -3,8 +3,7 @@ package com.zebrunner.carina.demo.saucedemo;
 import com.zebrunner.agent.core.annotation.TestLabel;
 import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
-import com.zebrunner.carina.demo.gui.saucedemo.LoginPage;
-import com.zebrunner.carina.demo.gui.saucedemo.MainPage;
+import com.zebrunner.carina.demo.gui.saucedemo.*;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -13,13 +12,25 @@ import org.testng.asserts.SoftAssert;
 public class MainPageTest implements IAbstractTest {
     LoginPage loginPage;
     MainPage mainPage;
+
+    CartPage cartPage;
+    CheckPage checkPage;
+    Check_two_Page check_two_page;
+    Check_Complete_Page checkCompletePage;
+
     @BeforeTest
     public void startDriver() {
         loginPage = new LoginPage(getDriver());
         mainPage = new MainPage(getDriver());
+        checkPage = new CheckPage(getDriver());
+        check_two_page = new Check_two_Page(getDriver());
+        cartPage = new CartPage(getDriver());
+        checkCompletePage = new Check_Complete_Page(getDriver());
     }
 
     @Test
+    @MethodOwner(owner = "dxl")
+    @TestLabel(name = "1. add item to cart and check cart change", value = {"web"})
     public void testCartValidation() {
         loginPage.open();
         SoftAssert softAssert = new SoftAssert();
@@ -28,10 +39,81 @@ public class MainPageTest implements IAbstractTest {
         loginPage.inputPassword("secret_sauce");
         loginPage.clickLoginButton();
         softAssert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Redirected URL after login is incorrect");
-        mainPage.clickAddToCartButton("sauce-labs-bike-light");
-        Assert.assertTrue(mainPage.isItemInCart("sauce-labs-bike-light"), "Item was not added to the cart!");
+        mainPage.clickAddToCartButton(0);
+        Assert.assertTrue(mainPage.isItemInCart(), "Item was not added to cart");
     }
 
+    @Test(dependsOnMethods = "testCartValidation")
+    @MethodOwner(owner = "dxl")
+    @TestLabel(name = "2. test product card", value = {"web"})
+    public void testProductCard() {
+        Assert.assertTrue(mainPage.isInventoryListPresent(), "Inventory list is not present or empty");
+    }
 
+    @Test(dependsOnMethods = "testProductCard")
+    @MethodOwner(owner = "dxl")
+    @TestLabel(name = "3. sort Z to A", value = {"web"})
+    public void testSortByNameZToA() throws InterruptedException {
+        mainPage.clickSortButton();
+        mainPage.selectSortOption("za");
+        Assert.assertTrue(mainPage.isSorted("za"), "Items are not sorted by name (Z to A)");
+    }
+
+    @Test(dependsOnMethods = "testSortByNameZToA")
+    @MethodOwner(owner = "dxl")
+    @TestLabel(name = "4. sort A to Z", value = {"web"})
+    public void testSortByNameAToZ() throws InterruptedException {
+        mainPage.clickSortButton();
+        mainPage.selectSortOption("az");
+        Assert.assertTrue(mainPage.isSorted("az"), "Items are not sorted by name (A to Z)");
+    }
+
+    @Test(dependsOnMethods = "testSortByNameAToZ")
+    @MethodOwner(owner = "dxl")
+    @TestLabel(name = "5. Sort low to high", value = {"web"})
+    public void testSortByPriceLowToHigh() throws InterruptedException {
+        mainPage.clickSortButton();
+        mainPage.selectSortOption("lohi");
+        Assert.assertTrue(mainPage.isSorted("lohi"), "Items are not sorted by price (low to high)");
+    }
+
+    @Test(dependsOnMethods = "testSortByPriceLowToHigh")
+    @MethodOwner(owner = "dxl")
+    @TestLabel(name = "6. Sort high to low", value = {"web"})
+    public void testSortByPriceHighToLow() throws InterruptedException {
+        mainPage.clickSortButton();
+        mainPage.selectSortOption("hilo");
+        Assert.assertTrue(mainPage.isSorted("hilo"), "Items are not sorted by price (high to low)");
+    }
+
+    @Test(dependsOnMethods = "testSortByPriceHighToLow")
+    @MethodOwner(owner = "dxl")
+    @TestLabel(name = "7. Open All site", value = {"web"})
+    public void testOpenOfAllPage() {
+        loginPage.open();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(loginPage.isPageOpened(), "Login Page Fail to opened");
+        loginPage.inputLogin("standard_user");
+        loginPage.inputPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        softAssert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Redirected URL after login is incorrect");
+        mainPage.clickAddToCartButton(0);
+        mainPage.clickCartButton();
+        softAssert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/cart.html", "Redirected URL after login is incorrect");
+        checkPage.goToCartPage();
+        softAssert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/checkout-step-one.html", "Redirected URL after login is incorrect");
+        check_two_page.enterFirstName("John");
+        check_two_page.enterLastName("John");
+        check_two_page.enterPostalCode("11222");
+        check_two_page.clickContinue();
+        softAssert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/checkout-step-two.html", "Redirected URL after login is incorrect");
+        cartPage.clickFinish();
+        softAssert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/checkout-complete.html", "Redirected URL after login is incorrect");
+        checkCompletePage.clickBackButton();
+        mainPage.openMenu();
+        mainPage.openAboutSite();
+        softAssert.assertEquals(getDriver().getCurrentUrl(), "https://saucelabs.com/", "About site did not open correctly");
+        softAssert.assertAll();
+    }
 
 }
